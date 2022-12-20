45629e500b65650dc6803a1964401efae5ddb995
Annisa Muja Ahidah <annisama2611@gmail.com>
2022-12-20 17:40:39 +0700

    update

---
+++ app/src/main/java/edu/upi/cs/yudiwbs/uas_template/FragmentDua.java
@@ -0,0 +1,114 @@
+package edu.upi.cs.yudiwbs.uas_template;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import android.hardware.SensorEventListener;
+
+import edu.upi.cs.yudiwbs.uas_template.databinding.FragmentDuaBinding;
+import edu.upi.cs.yudiwbs.uas_template.databinding.FragmentSatuBinding;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+
+public class FragmentDua extends Fragment implements SensorEventListener {
+
+    private FragmentDuaBinding binding;
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
+    ArrayList<Hasil> alHasil = new ArrayList<>();
+    AdapterHasil adapter;
+    RecyclerView.LayoutManager lm;
+
+
+    public FragmentDua() {
+        // Required empty public constructor
+    }
+
+    public static FragmentDua newInstance(String param1, String param2) {
+        FragmentDua fragment = new FragmentDua();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentDuaBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        adapter = new AdapterHasil(alHasil);
+        binding.rvHasil.setAdapter(adapter);
+
+        lm = new LinearLayoutManager(getActivity());
+        binding.rvHasil.setLayoutManager(lm);
+
+        //supaya ada garis antar row
+        binding.rvHasil.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+
+        binding.buttonFrag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alHasil.add(new Hasil("HP diangkat!"));
+
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+        lm = new LinearLayoutManager(getActivity());
+
+//restore state recylerview
+//        if (vmFragmentSatu.getData().getValue()!=null) {
+//            lm.onRestoreInstanceState(vmUserPref.getrecViewState().getValue());
+//        }
+//        rvDosen.setLayoutManager(lm);
+
+        return view;
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+//        Sensor mySensor = sensorEvent.sensor;
+//        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+//        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+    public void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+}
