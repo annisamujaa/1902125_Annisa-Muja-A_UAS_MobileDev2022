885218d153aeb660736b3d1a599d5bd3eb85be3c
Annisa Muja Ahidah <annisama2611@gmail.com>
2022-12-20 17:17:08 +0700

    update

---
+++ app/src/main/java/edu/upi/cs/yudiwbs/uas_template/ViewModelFragmentSatu.java
@@ -0,0 +1,22 @@
+package edu.upi.cs.yudiwbs.uas_template;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ViewModelFragmentSatu extends ViewModel {
+    public MutableLiveData<String> data;
+    public ViewModelFragmentSatu() {
+        data = new MutableLiveData<String>();
+        data.setValue(null);
+    }
+
+    public LiveData<String> getData() {
+        return data;
+    }
+
+    public void setData (String state) {
+        data.setValue(state);
+    }
+}
+
